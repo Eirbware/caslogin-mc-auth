070258406840d8c85ab366fa704144f9744b5b0f
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-06-09 01:15:49 +0200

    added a way to get the current request's url

--- utils.php
+++ utils.php
@@ -53,3 +53,7 @@ function array_has_all_keys(array $array, ...$keys): bool{
     http_response_code($response_code);
     die($reason);
 }
+
+function get_current_request_url(): string{
+    return ($_SERVER["HTTPS"] ? "https://" : "http://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+}
