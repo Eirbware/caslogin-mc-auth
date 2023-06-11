03600468eadd1aeeec5c61f183b35737bb88c24a
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-06-11 16:25:15 +0200

    added a way to get all banned users.

--- Requests.php
+++ Requests.php
@@ -3,7 +3,7 @@
 enum Requests: string
 {
 	const SELECT_USERS = 'SELECT * FROM CASUSERS';
-	const SELECT_LOGGED = 'SELECT * FROM LOGGED';
+	const GET_LOGGED_NOT_BANNED = 'SELECT * FROM LOGGED WHERE user NOT IN (SELECT banned FROM BANS)';
 	const SELECT_ROLES = 'SELECT * FROM ROLES';
 	const SELECT_USERS_WITH_ROLES = "SELECT u.*, r.id as 'role' from CASUSERS u left outer join USER_ROLES ur on (u.login = ur.login) left outer join ROLES r on (ur.role = r.id)";
 	const SEARCH_CAS_USER_BY_LOGIN = "SELECT * from CASUSERS WHERE login LIKE :loginSearch";
@@ -12,6 +12,7 @@ enum Requests: string
 	const GET_LOGGED_BY_UUID = "SELECT * FROM LOGGED WHERE uuid = :uuid";
 	const LOG_USER = "INSERT INTO LOGGED VALUES (:user, :uuid)";
 	const SEARCH_NOT_EXPIRED_BAN_BY_USER = "SELECT b.banned, b.banner, b.reason, b.timestamp, b.expires FROM CASUSERS u INNER JOIN BANS b ON (u.login = b.banned) WHERE b.banned LIKE :userSearch AND (b.expires IS NULL OR b.expires > NOW()) ORDER BY b.timestamp DESC";
+	const GET_NOT_EXPIRED_BANS = "SELECT * FROM BANS WHERE (expires IS NULL OR expires > now()) ORDER BY timestamp DESC";
 	const BAN_USER = "INSERT INTO BANS (banned, banner, reason, timestamp, expires) VALUES (:banned, :banner, :reason, NOW(), :expires)";
 }
 
--- api/ban.php
+++ api/ban.php
@@ -1,4 +1,7 @@
 <?php
+
+use JetBrains\PhpStorm\NoReturn;
+
 require_once '../utils.php';
 require_once '../CasLoginPDO.php';
 require_once '../Requests.php';
@@ -41,22 +44,47 @@ function ban_user(CasLoginPDO $pdo): void
 	$smt->execute();
 }
 
-if ($_SERVER['REQUEST_METHOD'] != "POST")
-	die_with_http_code(405, "<h1>Bad Method</h1>");
+#[NoReturn] function handle_ban_user(){
+	$json = file_get_contents('php://input');
+	$_POST = json_decode($json, true);
+
+	if (!array_has_all_keys($_POST, "banner", "banned"))
+		die_with_http_code(400, "<h1>Bad Request</h1>");
+
+	$pdo = new CasLoginPDO();
+	check_banned_exists($pdo);
+	check_banner_exists($pdo);
+	check_expires_correct_timestamp();
+
+	ban_user($pdo);
+	die_with_http_code_json(200, ["success" => true]);
+}
+
+function handle_get_bans(){
+	$pdo = new CasLoginPDO();
+	$smt = $pdo->prepare(Requests::GET_NOT_EXPIRED_BANS);
+	$bans = [];
+	$smt->execute();
+	foreach ($smt as $row) {
+		$ban = new Ban();
+		$ban->bannedUser = $row['banned'];
+		$ban->banner = $row['banner'];
+		$ban->reason = $row['reason'];
+		$ban->timestamp = new DateTime($row['timestamp']);
+		$ban->expires = $row['expires'] === null ? null : new DateTime($row['expires']);
+		$bans[] = $ban;
+	}
+	die_with_http_code_json(200, ["success" => true, "bans" => $bans]);
+}
 
 header("Accept: application/json");
 
-$json = file_get_contents('php://input');
-$_POST = json_decode($json, true);
+if ($_SERVER['REQUEST_METHOD'] != "POST" && $_SERVER['REQUEST_METHOD'] != "GET")
+	die_with_http_code(405, "<h1>Bad Method</h1>");
 
-if (!array_has_all_keys($_POST, "banner", "banned"))
-	die_with_http_code(400, "<h1>Bad Request</h1>");
-
-$pdo = new CasLoginPDO();
-check_banned_exists($pdo);
-check_banner_exists($pdo);
-check_expires_correct_timestamp();
-
-ban_user($pdo);
-die_with_http_code_json(200, ["success" => true]);
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	handle_ban_user();
+}else{
+	handle_get_bans();
+}
 
