c09d101d06cb212f1c36dcd8b69a41d17251e2a3
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-06-14 04:09:57 +0200

    oops

--- api/users.php
+++ api/users.php
@@ -1,11 +1,9 @@
 <?php
 require_once '../auth_endpoint.php';
 require_once '../utils.php';
-require_once '../CasLoginPDO.php';
-require_once '../LoggedUser.php';
 require_once '../Requests.php';
 if($_SERVER['REQUEST_METHOD'] != "GET")
 	die_with_http_code(405, "<h1>Method not allowed</h1>");
 require_once '../bootstrap.php';
 global $entityManager;
-die_with_http_code_json(200, ["success" => true, $entityManager->getRepository(LoggedUser::class)->findAll()]);
+die_with_http_code_json(200, ["success" => true, "users" => $entityManager->getRepository(LoggedUser::class)->findAll()]);
