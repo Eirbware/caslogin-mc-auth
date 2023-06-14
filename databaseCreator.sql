8ed450613c24f1ee7e75f676e22b0c5cdc331962
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-06-14 18:05:58 +0200

    Updated databaseCreator.sql

--- databaseCreator.sql
+++ databaseCreator.sql
@@ -1,123 +1,65 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: casloginauth
--- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.22.04.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
-
---
--- Table structure for table `BANS`
---
-
-DROP TABLE IF EXISTS `BANS`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `BANS`
+create table CASUSERS
 (
-    `id`        int         NOT NULL AUTO_INCREMENT,
-    `banned`    varchar(64) NOT NULL,
-    `banner`    varchar(64)  DEFAULT NULL,
-    `reason`    varchar(128) DEFAULT NULL,
-    `timestamp` datetime    NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `banned` (`banned`),
-    KEY `banner` (`banner`),
-    CONSTRAINT `BANS_ibfk_1` FOREIGN KEY (`banned`) REFERENCES `CASUSERS` (`login`),
-    CONSTRAINT `BANS_ibfk_2` FOREIGN KEY (`banner`) REFERENCES `CASUSERS` (`login`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    login varchar(64) not null
+        primary key,
+    ecole varchar(64) null
+);
 
---
--- Table structure for table `CASUSERS`
---
-
-DROP TABLE IF EXISTS `CASUSERS`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `CASUSERS`
+create table BANS
 (
-    `login` varchar(64) NOT NULL,
-    PRIMARY KEY (`login`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    id        int auto_increment
+        primary key,
+    banned    varchar(64)  not null,
+    banner    varchar(64)  null,
+    reason    varchar(128) null,
+    timestamp datetime     not null,
+    expires   datetime     null,
+    constraint BANS_ibfk_1
+        foreign key (banned) references CASUSERS (login),
+    constraint BANS_ibfk_2
+        foreign key (banner) references CASUSERS (login)
+);
 
---
--- Table structure for table `LOGGED`
---
+create index banned
+    on BANS (banned);
 
-DROP TABLE IF EXISTS `LOGGED`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `LOGGED`
+create index banner
+    on BANS (banner);
+
+create table LOGGED
 (
-    `user` varchar(64) NOT NULL,
-    `uuid` varchar(36) NOT NULL,
-    PRIMARY KEY (`user`),
-    UNIQUE KEY `uuid` (`uuid`),
-    CONSTRAINT `LOGGED_ibfk_1` FOREIGN KEY (`user`) REFERENCES `CASUSERS` (`login`),
-    CONSTRAINT `UUID_LEN_CHK` CHECK ((length(`UUID`) = 36))
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    id    int auto_increment
+        primary key,
+    login varchar(64) null,
+    uuid  varchar(36) null,
+    constraint login
+        unique (login),
+    constraint uuid
+        unique (uuid),
+    constraint LOGGED_ibfk_1
+        foreign key (login) references CASUSERS (login),
+    constraint USER_LEN_CHECK
+        check (length(`uuid`) = 36)
+);
 
---
--- Table structure for table `ROLES`
---
-
-DROP TABLE IF EXISTS `ROLES`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ROLES`
+create table ROLES
 (
-    `id` varchar(32) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    id varchar(32) not null
+        primary key
+);
 
---
--- Table structure for table `USER_ROLES`
---
-
-DROP TABLE IF EXISTS `USER_ROLES`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `USER_ROLES`
+create table USER_ROLES
 (
-    `login` varchar(64) NOT NULL,
-    `role`  varchar(32) NOT NULL,
-    PRIMARY KEY (`login`, `role`),
-    KEY `role` (`role`),
-    CONSTRAINT `USER_ROLES_ibfk_1` FOREIGN KEY (`login`) REFERENCES `CASUSERS` (`login`),
-    CONSTRAINT `USER_ROLES_ibfk_2` FOREIGN KEY (`role`) REFERENCES `ROLES` (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+    login varchar(64) not null,
+    role  varchar(32) not null,
+    primary key (login, role),
+    constraint USER_ROLES_ibfk_1
+        foreign key (login) references CASUSERS (login),
+    constraint USER_ROLES_ibfk_2
+        foreign key (role) references ROLES (id)
+);
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+create index role
+    on USER_ROLES (role);
 
--- Dump completed on 2023-06-07  2:06:50
+INSERT INTO ROLES VALUES ('admin'), ('moderator')
