02783e2cf4763339b520c8ebb7a45befc2bac1c1
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-09-02 16:46:05 +0200

    made logging work for auto auth. Now, time to use some codes to avoid CSRF

--- api/login.php
+++ api/login.php
@@ -1,9 +1,11 @@
 <?php
 
 use Doctrine\ORM\EntityManager;
+use JetBrains\PhpStorm\NoReturn;
 
 require_once '../env.php';
 require_once '../utils.php';
+require_once '../Errors.php';
 if ($_SERVER['REQUEST_METHOD'] != "GET") {
 	http_response_code(405);
 	echo '<h1>Method not allowed</h1>';
@@ -21,7 +23,7 @@ if (!array_key_exists('ticket', $_GET)) {
 } else {
 	require_once '../bootstrap.php';
 	global $entityManager;
-	login_success($entityManager);
+	login_success($entityManager, $_GET["ticket"], $_GET["uuid"]);
 }
 
 function redirect_cas(): void
@@ -31,11 +33,65 @@ function redirect_cas(): void
 	header("Location: " . $casUrl);
 }
 
-function login_success(EntityManager $entityManager): void
+function validate_cas_token(string $casToken, string $uuid): mixed
 {
-	$validationCode = create_auth_code($entityManager);
-	echo "Your validation code is <b>$validationCode</b>";
-	echo "<br/>You can keep the validation code and close the tab now.";
+    // Dirty but needed to not change code between prod and dev lol...)
+    if (!str_contains(get_env("cas_auth"), "cas.bordeaux-inp.fr")) {
+        $servUrl = rawurlencode(get_protocol() . $_SERVER["HTTP_HOST"] . "/api/login.php?uuid=$uuid");
+        $serviceUrl = get_env("cas_auth") . "?service=" . base64_encode($servUrl);
+    } else
+        $serviceUrl = get_current_request_url();
+    $validationUrl = get_env("cas_validate") . "?ticket=$casToken&service=$serviceUrl&format=json";
+    $ch = curl_init($validationUrl);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+
+    $resStr = curl_exec($ch);
+    curl_close($ch);
+    $res = json_decode($resStr, true);
+    if (array_key_exists("authenticationFailure", $res["serviceResponse"])) {
+        die_with_http_code_json(400, ["success" => false, "error" => Errors::INVALID_TOKEN]);
+    }
+    return $res;
+}
+
+function get_or_create_cas_user(EntityManager $entityManager, mixed $res): CasUser
+{
+    $userRepo = $entityManager->getRepository(CasUser::class);
+    $user = $userRepo->find($res["serviceResponse"]["authenticationSuccess"]["user"]);
+    if ($user === null) {
+        $user = new CasUser($res);
+        $entityManager->persist($user);
+        $entityManager->flush();
+    }
+    return $user;
+}
+
+function check_if_player_banned(EntityManager $entityManager, CasUser $casUser): void
+{
+    $banRepo = $entityManager->getRepository(Ban::class);
+    $ban = $banRepo->findOneBy(["banned" => $casUser->getLogin()]);
+    if ($ban !== null)
+        die_with_http_code_json(400, ["success" => false, "error" => Errors::USER_BANNED, "ban" => $ban]);
+}
+
+function login_player(EntityManager $entityManager, string $uuid, CasUser $casUser): LoggedUser
+{
+    $logged = new LoggedUser($casUser, $uuid);
+    $entityManager->persist($logged);
+    $entityManager->flush();
+    return $logged;
+}
+
+#[NoReturn] function login_success(EntityManager $entityManager, string $ticket, string $uuid): void
+{
+    $res = validate_cas_token($ticket, $uuid);
+    $casUser = get_or_create_cas_user($entityManager, $res);
+    check_if_player_banned($entityManager, $casUser);
+//    check_if_player_logged_in($entityManager, $casUser);
+    login_player($entityManager, $uuid, $casUser);
+    die_with_http_code(200, "Successfully logged in. Please wait");
 }
 
 function create_auth_code(EntityManager $entityManager): string
--- api/users.php
+++ api/users.php
@@ -2,7 +2,18 @@
 require_once '../auth_endpoint.php';
 require_once '../utils.php';
 if ($_SERVER['REQUEST_METHOD'] != "GET")
-	die_with_http_code(405, "<h1>Method not allowed</h1>");
+    die_with_http_code(405, "<h1>Method not allowed</h1>");
 require_once '../bootstrap.php';
 global $entityManager;
-die_with_http_code_json(200, ["success" => true, "users" => $entityManager->getRepository(LoggedUser::class)->findAll()]);
+$loggedUserRep = $entityManager->getRepository(LoggedUser::class);
+function get_user_with_uuid(string $uuid, \Doctrine\ORM\EntityRepository $loggedUserRep)
+{
+    $user = $loggedUserRep->findOneBy(["uuid" => $uuid]);
+    die_with_http_code_json(200, ["success" => true, "user" => $user]);
+}
+
+if (isset($_GET['uuid'])) {
+    get_user_with_uuid($_GET['uuid'], $loggedUserRep);
+} else {
+    die_with_http_code_json(200, ["success" => true, "users" => $loggedUserRep->findAll()]);
+}
